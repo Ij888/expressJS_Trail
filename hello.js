c418d88ff235911de735960ca0993610b0870317
Ij888 <ij.ofili@gmail.com>
2015-01-06 03:13:20 +0000

    added entry
    added entry
    now lets start the server

--- hello.js
+++ hello.js
@@ -5,3 +5,7 @@ var port = 3000;
 app.get('*', function(request, response){
     response.end('BufferStream_k177'); 
 });
+
+app.listen(port, function(){
+    console.log('the server done started, hit http://localhost:3000 on yer brwser')
+})
